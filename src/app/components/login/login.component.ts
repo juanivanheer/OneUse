8c7df8e0fa89bc160138d71d055ea16f3b9f4fb5
Juan Van Heerden <juanivanheer@gmail.com>
2021-03-09 21:22:28 -0300

    * Modificadas las interfacesl login + register * Agregado reCAPTCHA en login + register * Agregada opción facebook + google en login (falta backend) * Agregado certificados SSL para localhost (npm run start)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Output, EventEmitter, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { MatSnackBar } from '@angular/material';
 import { SingletonService } from '../singleton.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 declare const gapi: any;
+declare const FB: any;
 
 @Component({
   selector: 'app-login',
@@ -13,20 +15,68 @@ declare const gapi: any;
 
 export class LoginComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('recaptcha', { static: true }) recaptchaElement: ElementRef;
   loginUserData = { email: undefined, password: undefined }
   estado: boolean;
   public auth2: any;
+  recaptcha: boolean = false;
+  btn_habilitado: boolean = false;
 
-  constructor(private _auth: AuthService, private _router: Router, private _snackBar: MatSnackBar, private singleton: SingletonService) { }
+
+  constructor(private http: HttpClient, private _auth: AuthService, private _router: Router, private _snackBar: MatSnackBar, private singleton: SingletonService) { }
 
   ngOnInit() {
-
+    this.addRecaptchaScript();
+    setTimeout(() => {
+      document.getElementsByTagName("span")[17].textContent = "Acceder con Google";
+    }, 600);
   }
 
   ngAfterViewInit() {
     this.googleInit();
+    this.facebookInit();
+    console.log(gapi);
+    console.log(FB);
   }
 
+  lostPassword() {
+    window.location.assign("/lostpassword")
+  }
+
+  register() {
+    window.location.assign("/register")
+  }
+
+  /* INICIO DE SESIÓN FACEBOOK */
+  public facebookInit() {
+    (window as any).fbAsyncInit = function () {
+      window['FB'].init({
+        appId: '235541454927521',
+        cookie: true,
+        xfbml: true,
+        version: 'v3.1'
+      });
+      window['FB'].AppEvents.logPageView();
+    };
+  }
+
+  public loginFacebook() {
+    window['FB'].login((response) => {
+      console.log('login response', response);
+      if (response.authResponse) {
+        window['FB'].api('/me', {
+          fields: 'last_name, first_name, email'
+        }, (userInfo) => {
+          console.log("user information");
+          console.log(userInfo);
+        });
+      } else {
+        console.log('User login failed');
+      }
+    }, { scope: 'email' });
+  }
+
+  /* INICIO DE SESIÓN GOOGLE */
   public googleInit() {
     gapi.load('auth2', () => {
       this.auth2 = gapi.auth2.init({
@@ -38,10 +88,12 @@ export class LoginComponent implements OnInit, AfterViewInit {
     });
   }
 
+
   public attachSignin(element) {
     this.auth2.attachClickHandler(element, {},
       (googleUser) => {
-        console.log(googleUser)
+        console.log(googleUser);
+
         let profile = googleUser.getBasicProfile();
         console.log('Token || ' + googleUser.getAuthResponse().id_token);
         console.log('ID: ' + profile.getId());
@@ -52,13 +104,13 @@ export class LoginComponent implements OnInit, AfterViewInit {
 
 
       }, (error) => {
-        alert(JSON.stringify(error, undefined, 2));
+        console.log(JSON.stringify(error, undefined, 2));
       });
   }
 
 
 
-
+  /* INICIO DE SESIÓN ONEUSE */
   iniciarSesion() {
     if (this.estado) {
       this.singleton.setInicioSesion(true);
@@ -72,6 +124,64 @@ export class LoginComponent implements OnInit, AfterViewInit {
       duration: 8000,
       panelClass: ['color-snackbar']
     });
+  }
+
+  /* RECAPTCHA */
+  addRecaptchaScript() {
+    window['grecaptchaCallback'] = () => {
+      this.renderReCaptcha();
+    }
+    (function (d, s, id, obj) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s); js.id = id;
+      js.src = "https://www.google.com/recaptcha/api.js?onload=grecaptchaCallback&amp;render=explicit";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'recaptcha-jssdk', this));
+  }
+
+  renderReCaptcha() {
+    window['grecaptcha'].render(this.recaptchaElement.nativeElement, {
+      'sitekey': '6Lc23XgaAAAAAJ2AaNU4HSksDysKIQEtPyrCpJPr ',
+      'callback': (response) => {
+        var secret = "6Lc23XgaAAAAAHWgaIEqeN-8jjqEko9XVMNY7qkC";
+        this.http.post<any>("https://cors-localhost.herokuapp.com/https://www.google.com/recaptcha/api/siteverify?secret=" + secret + "&response=" + response, {}).subscribe(
+          res => {
+            if (res.success == true) {
+              this.recaptcha = true;
+            }
+            else this.recaptcha = false;
+            this.habilitarInicio();
+          },
+          err => {
+            console.log(err)
+          }
+        )
+        this.habilitarInicio();
+      },
+      'expired-callback': (response) => {
+        if(response == undefined) {}
+        this.recaptcha = false;
+        this.habilitarInicio();
+      }
+    });
+    this.habilitarInicio();
+  }
+
+  habilitarInicio() {
+    if (this.loginUserData.email != undefined && this.loginUserData.password != undefined) {
+      if (this.recaptcha == true) {
+        this.btn_habilitado = true;
+        document.querySelectorAll("button")[10].disabled = false;
+      }
+      else {
+        this.btn_habilitado = false;
+        document.querySelectorAll("button")[10].disabled = true;
+      }
+    } else {
+      this.btn_habilitado = false;
+      document.querySelectorAll("button")[10].disabled = true;
+    }
   }
 
   // onLogin(form): void {
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { MatSnackBar } from '@angular/material';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'appr-register',
@@ -17,15 +19,18 @@ export class RegisterComponent implements OnInit {
   minPw = 8;
   formGroup: FormGroup;
   appregister: FormGroup;
-  registerUserData = {email: undefined, password: undefined, name: undefined}
+  registerUserData = { email: undefined, password: undefined, name: undefined }
+  recaptcha: boolean = false;
+  habilitado: boolean = false;
 
   emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   passwordPattern: " ^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}$";
+  @ViewChild('recaptcha', { static: true }) recaptchaElement: ElementRef;
 
-
-  constructor(private _auth: AuthService, private FormBuilder: FormBuilder, private _router: Router, private _snackBar: MatSnackBar) { }
+  constructor(private http: HttpClient, private _auth: AuthService, private FormBuilder: FormBuilder, private _router: Router, private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.addRecaptchaScript();
     this.formGroup = this.FormBuilder.group({
       password: ['', [Validators.required]],
       password2: ['', [Validators.required]],
@@ -44,9 +49,59 @@ export class RegisterComponent implements OnInit {
       err => {
         //console.log(err);
         this.openSnackBar("Error en la base de datos. Probar en otro momento", "Aceptar");
-      } 
+      }
     )
     //console.log(this.registerUserData)
+  }
+
+  /* RECAPTCHA */
+  addRecaptchaScript() {
+    window['grecaptchaCallback'] = () => {
+      this.renderReCaptcha();
+    }
+    (function (d, s, id, obj) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s); js.id = id;
+      js.src = "https://www.google.com/recaptcha/api.js?onload=grecaptchaCallback&amp;render=explicit";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'recaptcha-jssdk', this));
+  }
+
+  renderReCaptcha() {
+    window['grecaptcha'].render(this.recaptchaElement.nativeElement, {
+      'sitekey': '6Lc23XgaAAAAAJ2AaNU4HSksDysKIQEtPyrCpJPr ',
+      'callback': (response) => {
+        var secret = "6Lc23XgaAAAAAHWgaIEqeN-8jjqEko9XVMNY7qkC";
+        this.http.post<any>("https://cors-localhost.herokuapp.com/https://www.google.com/recaptcha/api/siteverify?secret=" + secret + "&response=" + response, {}).subscribe(
+          res => {
+            if (res.success == true) {
+              this.recaptcha = true;
+            }
+            else this.recaptcha = false;
+            this.habilitarRegistrar();
+          },
+          err => {
+            console.log(err)
+          }
+        )
+        this.habilitarRegistrar();
+      },
+      'expired-callback': (response) => {
+        this.recaptcha = false;
+        this.habilitarRegistrar();
+        document.querySelectorAll("button")[12].disabled = true;
+      }
+    });
+    this.habilitarRegistrar();
+  }
+
+  lostPassword() {
+    window.location.assign("/lostpassword")
+  }
+
+  login() {
+    window.location.assign("/login")
   }
 
   openSnackBar(message: string, action: string) {
@@ -65,6 +120,18 @@ export class RegisterComponent implements OnInit {
     }
   }
 
+  habilitarRegistrar() {
+    if (this.formGroup.status == 'VALID') {
+      if (this.recaptcha == true) {
+        this.habilitado = true;
+        document.querySelectorAll("button")[12].disabled = false;
+      }
+    } else {
+      this.habilitado = false;
+      document.querySelectorAll("button")[12].disabled = true;
+    }
+  }
+
 
   /* Shorthands for form controls (used from within template) */
   get password() { return this.formGroup.get('password'); }
@@ -73,10 +140,9 @@ export class RegisterComponent implements OnInit {
   get email() { return this.formGroup.get('email'); }
   /* Called on each input in either password field */
   onPasswordInput() {
-    if (this.formGroup.hasError('passwordMismatch'))
-      this.password2.setErrors([{ 'passwordMismatch': true }]);
-    else
-      this.password2.setErrors(null);
+    if (this.formGroup.hasError('passwordMismatch')) this.password2.setErrors([{ 'passwordMismatch': true }]);
+    else this.password2.setErrors(null);
+    this.habilitarRegistrar()
   }
 }
 
