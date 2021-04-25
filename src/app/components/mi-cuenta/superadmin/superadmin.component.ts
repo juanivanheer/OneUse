213ab47aa1119ac8ca87bc4f57ce20d711062acc
Juan Van Heerden <juanivanheer@gmail.com>
2021-04-25 17:03:41 -0300

    Cambios cambios  cambios cambios cambiosssss

--- src/app/components/mi-cuenta/superadmin/superadmin.component.ts
+++ src/app/components/mi-cuenta/superadmin/superadmin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { AuthService } from 'src/app/services/auth.service';
 import { SingletonService } from '../../singleton.service';
@@ -16,7 +17,7 @@ export interface Botones {
 })
 export class SuperadminComponent implements OnInit {
 
-  constructor(private spinner: NgxSpinnerService, private singleton: SingletonService, private _auth: AuthService) { }
+  constructor(private spinner: NgxSpinnerService, private route: Router, private _auth: AuthService) { }
 
   verificado = false;
 
@@ -46,14 +47,14 @@ export class SuperadminComponent implements OnInit {
     }
   }
 
-  cerrarSesion() {
-    this.singleton.cerrarSesion();
+  redirigir(url){
+    this.route.navigateByUrl(url)
   }
 
   botones: Botones[] = [
     { icono: "dashboard", texto: "Alquileres", link: "/superadmin/alquileres" },
     { icono: "report", texto: "Denuncias", link: "/superadmin/denuncias" },
-    { icono: "assessment", texto: "Estadísticas", link: "/superadmin/estadisticas" },
+    { icono: "assessment", texto: "Estadísticas", link: "/superadmin/estadisticas?est=alquiler-categoria" },
     { icono: "storefront", texto: "Publicaciones", link: "/superadmin/publicaciones" },
     { icono: "menu_book", texto: "Reclamos", link: "/superadmin/reclamos" },
     { icono: "account_box", texto: "Usuarios", link: "/superadmin/usuarios" },
